1062f971edb7adbe61d03873b304a21d1776678b
Kishore Kumar <eroshik@protonmail.com>
2019-07-01 19:58:32 +0530

    First commit

---
+++ keys.py
@@ -0,0 +1,4 @@
+TWILIO_ACCOUNT_SID = "AC74ffbeca73dce3cb7f41b0fa76f761e3"
+TWILIO_API_KEY = "283ea572a0b7a9c9c3ae195269522670"
+twilio_no = ""   #Add your twilio mobile number here
+mobile = ""  #Add the cell phone number that you want to reciee
